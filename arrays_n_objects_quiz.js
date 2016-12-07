f04fd00e0b97d6e9f752ab741583391555e2221e
Student <idk123@gmail.com>
2016-12-07 08:31:50 -1000

    modified:   arrays_n_objects_quiz.js

---
+++ arrays_n_objects_quiz.js
@@ -0,0 +1,27 @@
+var myArray = ["lol","weebzard",10, true];{
+	console.log(myArray[2]);
+}
+function sorusuru_sutone(arg) {
+	if (myArray == true) {
+		console.log(myArray[0,1,2]);
+	}
+	else console.log("THOU SHALT NOT PASS!")
+}
+
+arg(myArray)
+
+var batman = {
+	man : true;
+	utilityBelt : ["sword", "shotgun", "robin mace"];
+	vehicle : batmobile;
+	sidekick : Roobin
+}
+
+function spiderpig(urg) {
+	if (batman.man == true) {
+		console.log("Batman is ready with his " + batman.utilityBelt[1] + " and " batman.vehicle + " with his not so trusty sidekick, " + batman.sidekick);
+	}
+	else console.log("No, I'm the Bait");
+}
+
+urg(batman)
